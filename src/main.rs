3b9e08cb1da76a90efb0a25320562ac0a6728e42
Robbie Straw <drbawb@fatalsyntax.com>
2017-01-05 13:14:17 -0600

    calculate offset position based on time
    this integrates the scrolling speed of the box over time since last
    frame instead of a constant amount per frame.
    
    this allows the scroll animation to be more or less framerate
    independent.

--- src/main.rs
+++ src/main.rs
@@ -16,30 +16,33 @@ use glium::glutin::VirtualKeyCode as VKC;
 use input::Input;
 use render::{RenderGroup, RenderJob};
 
+static TARGET_FPS_MS: u64 = 1000 / 120;
+
+enum Direction { Up, Right, Down, Left }
+
+fn dt2ms(dt: Duration) -> u64 {
+    (dt.as_secs() * 1000) + (dt.subsec_nanos() as u64 / 1_000_000)
+}
+
 struct ScrollyBox { ofs: [f32; 2] }
 impl ScrollyBox {
     pub fn new() -> Self {
         ScrollyBox { ofs: [0.0, 0.0] }
     }
 
-    pub fn up(&mut self) { 
-        self.ofs[1] += 0.01;
-        if self.ofs[1] >= 1.0 { self.ofs[1] = 0.0 }
-    }
+    pub fn update(&mut self, dt: Duration, dir: Direction) {
+        let scroll_v = 0.001; // (.001px * 1000ms) = 1 texture height / sec.
+        let (vx, vy) = match dir {
+            Direction::Up    => (      0.0,  scroll_v),
+            Direction::Down  => (      0.0, -scroll_v),
+            Direction::Left  => (-scroll_v,       0.0),
+            Direction::Right => ( scroll_v,       0.0),
+        };
 
-    pub fn down(&mut self) { 
-        self.ofs[1] -= 0.01;
-        if self.ofs[1] <= 0.0 { self.ofs[1] = 1.0 }
-    }
-
-    pub fn left(&mut self) { 
-        self.ofs[0] -= 0.01;
-        if self.ofs[0] <= 0.0 { self.ofs[0] = 1.0 }
-    }
-
-    pub fn right(&mut self) { 
-        self.ofs[0] += 0.01;
-        if self.ofs[0] >= 1.0 { self.ofs[0] = 0.0 }
+        // TODO: real vectors ...
+        // integrate velocity over time => offset distance
+        self.ofs[0] += vx * dt2ms(dt) as f32;
+        self.ofs[1] += vy * dt2ms(dt) as f32;
     }
 
     pub fn draw(&self, jobs: &mut Vec<RenderJob>) {
@@ -76,12 +79,13 @@ fn main() {
     let mut block = ScrollyBox::new();
 
     // game clock
-    let target_fps = Duration::from_millis(1000 / 60);
-    let mut frame_start;
+    let target_fps = Duration::from_millis(TARGET_FPS_MS);
+    let mut frame_start = Instant::now();
 
     println!("starting game loop ...");
     'runloop: loop {
         // top of frame
+        let frame_dt = Instant::now() - frame_start;
         frame_start = Instant::now();
         controller.begin_new_frame();
         render_jobs.clear();
@@ -106,10 +110,10 @@ fn main() {
         if controller.was_key_pressed(VKC::Escape) { break 'runloop }
 
         // scroll square interior
-        if controller.is_key_held(VKC::Up)    { block.up();    }
-        if controller.is_key_held(VKC::Right) { block.right(); }
-        if controller.is_key_held(VKC::Down)  { block.down();  }
-        if controller.is_key_held(VKC::Left)  { block.left();  }
+        if controller.is_key_held(VKC::Up)    { block.update(frame_dt, Direction::Up)    }
+        if controller.is_key_held(VKC::Right) { block.update(frame_dt, Direction::Right) }
+        if controller.is_key_held(VKC::Down)  { block.update(frame_dt, Direction::Down)  }
+        if controller.is_key_held(VKC::Left)  { block.update(frame_dt, Direction::Left)  }
 
         // prepare render queue
         render_jobs.push(RenderJob::ClearScreen(0.0, 0.0, 0.0, 1.0));
@@ -121,9 +125,9 @@ fn main() {
         renderer.draw(&render_jobs[..], &mut frame);
         frame.finish().unwrap();
 
+        // handle timing
         let dt = (Instant::now()).duration_since(frame_start);
-        if dt > target_fps { println!("missed frame"); continue }
-
+        if dt > target_fps { println!("missed frame {:?}", dt); continue }
         let draw_time = target_fps - dt;
         thread::sleep(draw_time);
     }
