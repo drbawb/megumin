4c08d9b58d7e34d88447def5d56beedf52709708
Robbie Straw <drbawb@fatalsyntax.com>
2017-01-06 10:04:03 -0600

    pull out solid color texture filling

--- src/entities/map.rs
+++ src/entities/map.rs
@@ -11,37 +11,10 @@ impl TileMap {
     pub fn new(display: &mut RenderGroup) -> Self {
         let mut buf = vec![vec![(0,0,0,0); 64]; 64];
 
-        let rtex = {
-            for row in 0..64 {
-                for col in 0..64 { buf[row][col] = (255,0,0,255) }
-            }
-            
-            display.store_texture(buf.clone())
-        };
-
-        let gtex = {
-            for row in 0..64 {
-                for col in 0..64 { buf[row][col] = (0,255,0,255) }
-            }
-            
-            display.store_texture(buf.clone())
-        };
-
-        let btex = {
-            for row in 0..64 {
-                for col in 0..64 { buf[row][col] = (0,0,255,255) }
-            }
-            
-            display.store_texture(buf.clone())
-        };
-
-        let ytex = {
-            for row in 0..64 {
-                for col in 0..64 { buf[row][col] = (255,255,0,255) }
-            }
-            
-            display.store_texture(buf.clone())
-        };
+        let rtex = display.store_texture(fill_64_64(&mut buf, (255,0,0,255)));
+        let gtex = display.store_texture(fill_64_64(&mut buf, (0,255,0,255)));
+        let btex = display.store_texture(fill_64_64(&mut buf, (0,0,255,255)));
+        let ytex = display.store_texture(fill_64_64(&mut buf, (255,255,0,255)));
 
         TileMap { rtex: rtex, gtex: gtex, btex: btex, ytex: ytex }
     }
@@ -54,4 +27,12 @@ impl TileMap {
         jobs.push(RenderJob::TexRect(self.btex, 0.5, 0.0, w, h));
         jobs.push(RenderJob::TexRect(self.ytex, 0.5, 0.5, w, h));
     }
+}
+
+fn fill_64_64(buf: &mut Vec<Vec<(u8,u8,u8,u8)>>, color: (u8,u8,u8,u8)) -> Vec<Vec<(u8,u8,u8,u8)>> {
+    for row in 0..64 {
+        for col in 0..64 { buf[row][col] = color }
+    }
+
+    buf.clone()
 }
